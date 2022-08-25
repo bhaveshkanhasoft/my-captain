f5828da8fa53ae701ea54f8730593b775cc12b4d
bhaveshkanhasoft <110833600+bhaveshkanhasoft@users.noreply.github.com>
2022-08-25 16:54:45 +0530

    Update BookingsController.php
    Indentation corrected in controller

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -18,7 +18,7 @@ class BookingsController extends Controller
 	 */
 	public static function read(): array
 	{
-        return Bookings::readAllBookings();
+        	return Bookings::readAllBookings();
 	}
 
 	/**
@@ -29,26 +29,26 @@ class BookingsController extends Controller
 	 */
 	public function create( Request $request ): JsonResponse
 	{
-        $validate_field = array( Bookings::DATE_LABEL => $request->date, Bookings::NUMBER_OF_GUESTS_LABEL => $request->numOfGuests );
-        $validator = Validator::make( $validate_field, [
-            Bookings::DATE_LABEL => 'required|bookable_date',
-            Bookings::NUMBER_OF_GUESTS_LABEL => 'required|max:8',
-        ],
-        [
-            'date.bookable_date'=> 'You can not proceed with the selected booking date, please select another date.',
-        ]);
+		$validate_field = array( Bookings::DATE_LABEL => $request->date, Bookings::NUMBER_OF_GUESTS_LABEL => $request->numOfGuests );
+		$validator = Validator::make( $validate_field, [
+			Bookings::DATE_LABEL => 'required|bookable_date',
+			Bookings::NUMBER_OF_GUESTS_LABEL => 'required|max:8',
+        	],
+        	[
+			'date.bookable_date'=> 'You can not proceed with the selected booking date, please select another date.',
+		]);
 
 		if ( $validator->fails() ) {
 			return response()->json( [ 'msg' => $validator->errors()->first() ] );
 		}
 
-        $currentBookings = Bookings::getCurrentBookings();
-        $values = $request->all();
-        $values[ 'created_at' ] = Carbon::now()->format( 'Y-m-d' );
-        $currentBookings[] = $values;
-        $msg = "Booking has been made successfully.";
+		$currentBookings = Bookings::getCurrentBookings();
+		$values = $request->all();
+		$values[ 'created_at' ] = Carbon::now()->format( 'Y-m-d' );
+		$currentBookings[] = $values;
+		$msg = "Booking has been made successfully.";
 		Redis::set( Bookings::CACHE_KEY, json_encode( $currentBookings ) );
-
+		
 		return response()->json( [ 'msg' => $msg ] );
 	}
 }
